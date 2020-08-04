9a0e8167e9c3a48b875f9048050ed5e095dce439
leekpux <leekpux@126.com>
2020-08-04 16:56:57 +0800

    implicit type

--- src/parse.js
+++ src/parse.js
@@ -1,75 +1,61 @@
-var exceljs = require('exceljs');
-var async = require('async');
-var config = require('./config');
+const exceljs = require('exceljs');
+const async = require('async');
+const config = require('./config');
 
 function parseExcel(filePath, callback) {
     console.log('===================================');
     console.log('===================================');
     console.log('===================================');
     console.log('parsing excel: ', filePath);
-    var data = {};
-    var workbook = new exceljs.Workbook();
+    const data = {};
+    const workbook = new exceljs.Workbook();
     workbook.xlsx.readFile(filePath)
-        .then(function() {
-            var worksheets = workbook.worksheets;
-            worksheets.forEach(function(sheet) {
-                var name = sheet.name;
-                console.log('===================================');
-                console.log('parsing sheet: ', name);
-                var sheetData = parseSheet(sheet);
-                console.log('sheetData: ', sheetData);
-                data[name] = sheetData;
-            })
-            console.log('data: ', data);
+        .then(function (onfullfilled, onrejected) {
+            const worksheets = workbook.worksheets;
+            try {
+                worksheets.forEach(function (sheet) {
+                    const name = sheet.name;
+                    console.log('===================================');
+                    console.log('parsing sheet: ', name);
+                    const sheetData = parseSheet(sheet);
+                    // console.log('sheetData: ', sheetData);
+                    data[name] = sheetData;
+                })
+
+            } catch (e) {
+                console.log(e)
+                onrejected(e)
+            }
+            // console.log('data: ', data);
             callback(null, data);
         })
-        .catch(function(e) {
+        .catch(function (e) {
             callback(e);
         })
 }
 
 function parseExcels(filePaths, callback) {
-    async.map(filePaths, parseExcel, function(err, datas) {
+    async.mapSeries(filePaths, parseExcel, function (err, datas) {
         callback(err, datas);
     })
 }
 
-// function parseExcels(filePaths, callback) {
-//     var map = {};
-//     var datas = [];
-//     filePaths.forEach(function(filePath, idx) {
-//         map[filePath] = idx;
-//     })
-//     async.forEachOf(map, function(idx, filePath, cb) {
-//         parseExcel(filePath, function(err, data) {
-//             if (err) {
-//                 cb(err);
-//                 return;
-//             }
-//             datas[idx] = data;
-//             cb(null, data);
-//         });
-//     }, function(err, datas) {
-//         callback(err, datas);
-//     })
-// }
-
 function parseSheet(worksheet) {
-    var firstRow = worksheet.getRow(1);
+    const firstRow = worksheet.getRow(1);
     if (firstRow.getCell(1).value === 'key' && firstRow.getCell(2).value === 'value' && firstRow.getCell(3).value === 'type') {
-        console.log('type: map');
+        // console.log('type: map');
         return parseMap(worksheet);
     }
-    console.log('type: array');
+    // console.log('type: array');
     return parseArray(worksheet);
 }
 
 function parseMap(worksheet) {
-    var data = {};
-    var key;
-    var value;
-    var type;
-    worksheet.eachRow(function(row, rowNumber) {
+    const data = {};
+    let key;
+    let value;
+    let type;
+    worksheet.eachRow(function (row, rowNumber) {
         if (rowNumber <= 1) {
             return;
         }
@@ -81,7 +67,7 @@ function parseMap(worksheet) {
         }
         try {
             data[key] = parseValue(value, type);
-        } catch(e) {
+        } catch (e) {
             console.error('worksheet: ' + worksheet.name + ' row: ' + rowNumber + ' parse failed! ');
             console.error('key: ' + key + ' , value: ' + value + ' , type: ' + type);
             throw e;
@@ -91,52 +77,59 @@ function parseMap(worksheet) {
 }
 
 function parseArray(worksheet) {
-    var data = [];
+    const data = [];
 
-    var keys = [];
-    var options = config.getOptions()
-    var startRow = options.startRow || 1
-    worksheet.getRow(startRow).eachCell(function(cell, cellNumber) {
-        var key;
-        key = cell.text.trim()
-        if (!key) {
-            throw new Error('worksheet: ' + worksheet.name + ' key is empty!');
-        }
-        keys.push(key);
+    const keys = {};
+    const options = config.getOptions()
+    const startRow = options.startRow || 1
+    const startCol = options.startCol || 1
+    worksheet.getRow(startRow).eachCell(function (cell, cellNumber) {
+        if (cellNumber < startCol) return;
+        keys[cellNumber] = cell.text.trim();
     })
 
-    console.log('PARSE_ARRAY: ', keys)
+    // console.log('PARSE_ARRAY: ', keys)
+    const hasExplicitType = checkArrayHasExplicitType(worksheet, startRow, startCol)
+    const dataStartRow = startRow + (hasExplicitType ? 2 : 1)
 
-    var types = [];
-    worksheet.getRow(startRow + 1).eachCell(function(cell, cellNumber) {
-        var type;
-        type = cell.text.trim();
+    const types = {};
+    if (!hasExplicitType) {
+        worksheet.getRow(startRow).eachCell(function (cell, cellNumber) {
+            if (cellNumber < startCol || !keys[cellNumber]) return;
 
-        if (!type) {
-            throw new Error('worksheet: ' + worksheet.name + ' type is empty!');
-        }
-        types.push(type);
-    })
+            types[cellNumber] = getColType(worksheet, cellNumber, dataStartRow)
+        })
+    } else {
+        worksheet.getRow(startRow + 1).eachCell(function (cell, cellNumber) {
+            if (cellNumber < startCol || !keys[cellNumber]) return;
 
-    var item;
-    worksheet.eachRow(function(row, rowNumber) {
-        if (rowNumber <= startRow + 1) {
+            types[cellNumber] = cell.text.trim()
+        })
+    }
+    // console.log(types)
+    // return data
+
+    worksheet.eachRow(function (row, rowNumber) {
+        if (rowNumber < dataStartRow) {
             return;
         }
 
-        item = {};
-        keys.forEach(function(key, keyIndex) {
-            var type = types[keyIndex];
-            var value = row.getCell(keyIndex + 1).text;
+        const item = {};
+        row.eachCell(function (cell, cellNumber) {
+            const key = keys[cellNumber];
+            const type = types[cellNumber];
+            if (!key || !type) return;
+
+            const value = cell.text;
             try {
                 item[key] = parseValue(value, type);
-            } catch(e) {
+            } catch (e) {
                 console.log(e);
                 console.error('worksheet: ' + worksheet.name + ' row: ' + rowNumber + ' parse failed! ');
                 console.error('key: ' + key + ' , value: ' + value + ' , type: ' + type);
                 throw e;
             }
-        });
+        })
         data.push(item);
     })
     return data;
@@ -149,47 +142,113 @@ function parseValue(value, type) {
     if (typeof value === 'number') {
         value = value.toString()
     }
-    switch(type) {
-      case 'int':
-        return parseInt(value);
-      case 'num':
-      case 'float':
-      case 'number':
-        return Number(value);
-      case 'time':
-        var m = /(-?)(?:(\d+)d)?(?:(\d+)h)?(?:(\d+)m)?(?:(\d+\.?\d*)s?)?/i.exec(value);
-        var time = 0;
-        var nagative = m[1];
-        if(m[2]) {
-            time += parseInt(m[2]) * 24 * 60 * 60 * 1000;
-        } else if(m[3]) {
-            time += parseInt(m[3]) * 60 * 60 * 1000;
-        } else if(m[4]) {
-            time += parseInt(m[4]) * 60 * 1000;
-        } else if(m[5]) {
-            time += Number(m[5]) * 1000;
-        }
-        return (nagative ? -1 : 1) * time;
-      case 'bool':
-        value = value && value.toLowerCase();
-        return value == 'yes' || value == 'true' || value == '是' || value == 'y' || value == '1';
-      case 'string':
-        return value;
-      default:
-        var m, t;
-        // int[]
-        m = /(.*)\[\]/.exec(type);
-        if(m) {
-            if(value == '') return [];
-            t = m[1];
-            value = value.replace(/，/g, ',');
-            return value.split(',').map(function(v) {
+    switch (type) {
+        case 'int':
+            return parseInt(value);
+        case 'num':
+        case 'float':
+        case 'number':
+            return Number(value);
+        case 'time':
+            const r = /(-?)(?:(\d+)d)?(?:(\d+)h)?(?:(\d+)m)?(?:(\d+\.?\d*)s?)?/i.exec(value);
+            const time = 0;
+            const nagative = r[1];
+            if (r[2]) {
+                time += parseInt(r[2]) * 24 * 60 * 60 * 1000;
+            } else if (r[3]) {
+                time += parseInt(r[3]) * 60 * 60 * 1000;
+            } else if (r[4]) {
+                time += parseInt(r[4]) * 60 * 1000;
+            } else if (r[5]) {
+                time += Number(r[5]) * 1000;
+            }
+            return (nagative ? -1 : 1) * time;
+        case 'bool':
+            value = value && value.toLowerCase();
+            return value == 'yes' || value == 'true' || value == '是' || value == 'y' || value == '1';
+        case 'string':
+            return value;
+        default:
+            let m, t;
+            // int[]
+            m = /(.*)\[\]/.exec(type);
+            if (m) {
+                if (value == '') return [];
+                t = m[1];
+                value = value.replace(/，/g, ',');
+                return value.split(',').map(function (v) {
                     return parseValue(v, t);
-            });
-        }
-        // default string;
-        return value;
+                });
+            }
+            // default string;
+            return value;
     }
+}
+
+function checkArrayHasExplicitType(worksheet, startRow, startCol) {
+    const typeNames = [
+        'number',
+        'int',
+        'float',
+        'time',
+        'bool',
+        'string',
+    ]
+
+    const type = worksheet.getRow(startRow + 1).getCell(startCol).text.trim()
+    for (let i = 0; i < typeNames.length; i++) {
+        if (type.indexOf(typeNames[i]) !== -1) {
+            return true
+        }
+    }
+
+    return false
+}
+
+function getColType(worksheet, col, dataStartRow) {
+    const rowCount = worksheet.rowCount
+    for (let i = dataStartRow; i <= rowCount; i++) {
+        const text = worksheet.getRow(i).getCell(col).text
+        if (text !== '') {
+            return inferCellType(text)
+        }
+    }
+
+    return null
+}
+
+function inferCellType(value) {
+    if (value === '') return null
+
+    if (value.indexOf(',') !== -1) {
+        const arr = value.split(',')
+        if (arr && arr[0]) return inferCellType(arr[0]) + '[]'
+    }
+
+    const numberValue = Number(value)
+    if (!isNaN(numberValue)) {
+        if (parseInt(value) === numberValue) {
+            return 'int'
+        } else {
+            return 'float'
+        }
+    }
+
+    const boolStrings = [
+        'yes',
+        'y',
+        'true',
+        '是',
+        '1',
+        'no',
+        'n',
+        'false',
+        '否',
+        '0',
+    ]
+    if (boolStrings.indexOf(value) !== -1) return 'bool'
+
+    return 'string'
 }
 
 module.exports = {
